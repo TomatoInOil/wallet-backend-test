a27763b3d3ccbd14b91924c340cb59ffc9cf7212
Daniil Pautov <danpautoff@yandex.ru>
2025-05-24 14:30:01 +0300

    Add Docker configuration.

--- src/backend/settings/dev.py
+++ src/backend/settings/dev.py
@@ -1,1 +1,18 @@
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+from .base import *  # noqa
+
+ALLOWED_HOSTS = ["backend"]
+CSRF_TRUSTED_ORIGINS = ("http://127.0.0.1", "https://127.0.0.1")
+
+DATABASES = {
+    "default": {
+        "ENGINE": os.getenv(
+            "DB_ENGINE",
+            default="django.db.backends.postgresql_psycopg2",
+        ),
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+    }
+}
